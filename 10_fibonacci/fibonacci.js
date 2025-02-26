30f26e2c1d531d329f747d9965bf5f962c898006
NotSoClevr <157626810+NotSoClevr@users.noreply.github.com>
2025-02-26 12:08:04 -0500

    Complete exercise requirements

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibArray = [0, 1, 1];
+    for (i = 3; i < 50; i++){
+        fibArray.push(fibArray[(i-1)] + fibArray[(i-2)])
+    };
+    if (parseInt(index) < 0) {
+        return "OOPS"
+    } else {
+        return fibArray[parseInt(index)]
+    }
 };
 
 // Do not edit below this line
