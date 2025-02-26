dd300206e642fdf5cb77b4a4364d23bf47e1524f
NotSoClevr <157626810+NotSoClevr@users.noreply.github.com>
2025-02-26 11:32:02 -0500

    Complete exercise requirements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //const punctuation = /[\.,?!]/g;
+    const uniform = str
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .toLowerCase()
+      .toString();
+    const reversed = uniform.split("").reverse().join("").toString();
+    console.log(uniform);
+    console.log(reversed);
+    if (uniform === reversed) {
+        return true
+    }else {return false}
+    
 };
 
 // Do not edit below this line
