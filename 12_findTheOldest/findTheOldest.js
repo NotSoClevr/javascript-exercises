78de906c6008e6230285fd81b35f1fa5e5a1cccd
NotSoClevr <157626810+NotSoClevr@users.noreply.github.com>
2025-02-26 13:13:05 -0500

    Complete exercise requirements

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,44 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    const oldest = arr.sort(function (a, b) {
+        // const last = a.yearOfDeath - a.yearOfBirth;
+        // const next = b.yearOfDeath - b.yearOfBirth;
+        const d = new Date();
+        const year = d.getFullYear();
+        if (!a.yearOfDeath && !b.yearOfDeath) {
+            let last = year - a.yearOfBirth;
+            let next = year - b.yearOfBirth;
+            if (last > next) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (!a.yearOfDeath && b.yearOfDeath) {
+            let last = year - a.yearOfBirth;
+            let next = b.yearOfDeath - b.yearOfBirth;
+            if (last > next) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (a.yearOfDeath && !b.yearOfDeath) {
+            let last = a.yearOfDeath - a.yearOfBirth;
+            let next = year - b.yearOfBirth;
+            if (last > next) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else {
+            let last = a.yearOfDeath - a.yearOfBirth;
+            let next = b.yearOfDeath - b.yearOfBirth;
+            if (last > next) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        
+    }); return oldest[0]
 };
 
 // Do not edit below this line
